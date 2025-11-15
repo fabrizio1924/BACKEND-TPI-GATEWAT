b25488a7090cb9dc5ee6782dd802590a7656f097
Fabrizio Spitale <fabrizio.angelo.spitale@gmail.com>
2025-11-15 14:59:05 -0300

    Add API Gateway

--- src/main/java/com/utn/frc/backend/api_gw_logistica_contenedores/ApiGwLogisticaContenedoresApplication.java
+++ src/main/java/com/utn/frc/backend/api_gw_logistica_contenedores/ApiGwLogisticaContenedoresApplication.java
@@ -9,5 +9,4 @@ public class ApiGwLogisticaContenedoresApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGwLogisticaContenedoresApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/utn/frc/backend/api_gw_logistica_contenedores/config/GWConfig.java
@@ -0,0 +1,22 @@
+package com.utn.frc.backend.api_gw_logistica_contenedores.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GWConfig
+{
+    public RouteLocator configurarRutas(RouteLocatorBuilder builder,
+                                        @Value("${apunte-api-gw-tpi.url-microservicio-solicitudes}")
+                                        String uriSoicitudes,
+                                        @Value("${apunte-api-gw-tpi.url-microservicio-rutas}")
+                                        String uriRutas) {
+        return builder.routes()
+                .route(p -> p.path("/api/solicitudes-app/**").uri(uriSoicitudes))
+                .route(p -> p.path("/api/rutas-app/**").uri(uriSoicitudes))
+                .build();
+    }
+}
