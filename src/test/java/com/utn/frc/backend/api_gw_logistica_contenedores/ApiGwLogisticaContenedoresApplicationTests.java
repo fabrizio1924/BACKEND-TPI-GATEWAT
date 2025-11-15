686dc86baa5ad210804e6568ef79ff120a37dd5a
Fabrizio Spitale <fabrizio.angelo.spitale@gmail.com>
2025-11-15 12:54:06 -0300

    First Commit

---
+++ src/test/java/com/utn/frc/backend/api_gw_logistica_contenedores/ApiGwLogisticaContenedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.frc.backend.api_gw_logistica_contenedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGwLogisticaContenedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
